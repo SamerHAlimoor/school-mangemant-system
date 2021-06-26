ed8918db87eb114bdc6c79b3bb56be6689402baf
Samer Alimoor <sipar.alamour@gmail.com>
2021-06-26 10:48:11 +0200

    26-06-2021--11:48PM

--- routes/web.php
+++ routes/web.php
@@ -71,6 +71,11 @@ Route::group(['prefix' => LaravelLocalization::setLocale(), 'middleware' => ['lo
         Route::get('/Get_classrooms/{id}', 'StudentController@Get_classrooms');
         Route::get('/Get_Sections/{id}', 'StudentController@Get_Sections');
         Route::get('Students/details/{id}', 'StudentController@Get_Details')->name('Students.details');
+//Attachment in student
+        Route::post('Upload_attachment', 'StudentController@Upload_attachment')->name('Upload_attachment');
+        Route::get('Download_attachment/{studentsname}/{filename}', 'StudentController@Download_attachment')->name('Download_attachment');
+        Route::post('Delete_attachment', 'StudentController@Delete_attachment')->name('Delete_attachment');
+
 
         
 
